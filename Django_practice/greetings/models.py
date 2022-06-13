6b9b821c07e34020dc640f603e365ab5104e4f06
kmaki4 <kmaki4@hawaii.edu>
2022-06-12 20:46:23 -1000

    Revert "Django Additions"
    This reverts commit ac50f12dbc485ff0ad025d8acaf852e9caa1992c.

--- Django_practice/greetings/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Greeting(models.Model):
-    message = models.CharField(max_length=200)
--- Django_practice/scripts/start_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#This script starts the application
-
-python manage.py runserver 0.0.0.0:8000
-
